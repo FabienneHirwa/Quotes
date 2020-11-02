ed0b25cfb625a9eb1f99ebc95b0ec05435e6815a
Fabienne Hirwa <fabiennehirwa1@gmail.com>
2020-11-02 13:18:27 -0500

    change

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
     new Quote(1, 'What you do not want done to yourself, do not do to others.','joselyne', 0,0,new Date(2020,8,20)),
     new Quote(2, 'A single rose can be my garden… a single friend, my world.','claudine',0,0,new Date(2020,3,14)),
-    new Quote(3,'There’s not a word yet for old friends who’ve just met.','Ritha',0,0,new Date(2020,6,18)),
+    new Quote(3,'There’s not  just met.','Ritha',0,0,new Date(2020,6,18)),
     new Quote (4, 'One’s friends are that part of the human race with which one can be human','Vivine',0,0,new Date(2020,9,25)),
      new Quote(5,' slow to fall into friendship; but when thou art in, continue firm & constant','Fabiene',0,0,new Date(2020,5,12)),
     new Quote (6,'the privilege of friendship to talk nonsense, and to have her nonsense respected.','Bonte',0,0,new Date(2020,7,3)),
