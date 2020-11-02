5dff3b80340ad5a187d20b89646149b76d0c34e9
Fabienne Hirwa <fabiennehirwa1@gmail.com>
2020-11-02 13:25:45 -0500

    change4

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,7 +12,7 @@ export class QuoteComponent implements OnInit {
     new Quote(2, 'Life is what happens when you’re busy making other plans.','Fabienne',0,0,new Date(2020,3,14)),
     new Quote(3,'Get busy living or get busy dying.','Claude',0,0,new Date(2020,6,18)),
     new Quote (4, 'You only live once, but if you do it right, once is enough.','Diane',0,0,new Date(2020,9,25)),
-     new Quote(5,' slow to fall into friendship; but when thou art in, continue firm & constant','Fabiene',0,0,new Date(2020,5,12)),
+     new Quote(5,'Many of life’s failures are people who did not realize how close they were to success when they gave up.','FRorberto',0,0,new Date(2020,5,12)),
     new Quote (6,'the privilege of friendship to talk nonsense, and to have her nonsense respected.','Bonte',0,0,new Date(2020,7,3)),
    ];
 
